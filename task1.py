6d3af4f46d7654d4fba95e451660b1454201f58b
NSPonomarev <118664438+NSPonomarev@users.noreply.github.com>
2023-01-30 22:20:30 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -13,8 +13,8 @@ BOOKS_DATABASE = [
 
 
 class Book:
-    def __init__(self, id_, name, pages):
-        self.id_ = id_
+    def __init__(self, id, name, pages):
+        self.id = id
         self.name = name
         self.pages = pages
 
@@ -22,13 +22,13 @@ class Book:
         return f'Книга "{self.name}"'
 
     def __repr__(self):
-        return f'Book(id_={self.id_}, name={self.name!r}, pages={self.pages})'
+        return f'Book(id={self.id}, name={self.name!r}, pages={self.pages})'
 
 
 if __name__ == '__main__':
     # инициализируем список книг
     list_books = [
-        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
+        Book(id=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
     ]
     for book in list_books:
         print(book)  # проверяем метод __str__
