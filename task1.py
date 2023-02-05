5de29c54942f34a01f253a5edc7bce73811ab90d
NSPonomarev <118664438+NSPonomarev@users.noreply.github.com>
2023-02-05 17:31:21 +0300

    Лабораторная работа 3

---
+++ task1.py
@@ -0,0 +1,52 @@
+class Book:
+    """ Базовый класс книги. """
+    def __init__(self, name: str, author: str):
+        self._name = name
+        self._author = author
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def author(self):
+        return self._author
+
+    def __str__(self) -> str:
+        return f"Книга {self.name}. Автор {self.author}"
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+
+
+class PaperBook(Book):
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)
+        if not isinstance(pages, int):
+            raise TypeError
+        if not pages > 0:
+            raise ValueError
+        self.pages = pages
+
+    def __str__(self) -> str:
+        return f"Книга {self.name}. Автор {self.author}. Страницы {self.pages}"
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, pages={self.pages!r})"
+
+
+class AudioBook(Book):
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)
+        if not isinstance(duration, float):
+            raise TypeError
+        if not duration > 0:
+            raise ValueError
+        self.duration = duration
+
+    def __str__(self) -> str:
+        return f"Книга {self.name}. Автор {self.author}. Продолжительность {self.duration}"
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, duration={self.duration!r})"
+#
